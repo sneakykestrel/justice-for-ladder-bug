8dba033bf5054d2f2a59dd0aef1f56db92ae6411
sneakykestrel <141267380+sneakykestrel@users.noreply.github.com>
2024-09-27 01:31:43 +0100

    Add project files.

---
+++ Justice For Ladder Bug/Mod.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.Mono;
+using HarmonyLib;
+using UnityEngine;
+
+
+namespace Justice_For_Ladder_Bug
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginversion)]
+    public class Mod : BaseUnityPlugin
+    {
+        public const string pluginGuid = "kestrel.iamyourbeast.justiceforladderbug";
+        public const string pluginName = "Justice For Ladder Bug";
+        public const string pluginversion = "1.0.0";
+
+        public static ConfigEntry<float> launchChance;
+        public static ConfigEntry<float> launchMultiplier;
+
+        public static ConfigEntry<float> shakeFrequency;
+        public static ConfigEntry<float> shakeScale;
+        public static ConfigEntry<float> shakeLength;
+
+        public static System.Random rand = new System.Random();
+
+        public void Awake() {
+
+            launchChance = Config.Bind("Launch", "Launch Chance", 10f, "The chance for a ladder to launch you, in percent (0-100)");
+            launchMultiplier = Config.Bind("Launch", "Launch Multiplier", 2700f, "A multiplier for the launch strength");
+
+            shakeFrequency = Config.Bind("Screenshake", "Screenshake Frequency", 7.5f, "The frequency of the screenshake effect");
+            shakeScale = Config.Bind("Screenshake", "Screenshake Scale", 100f, "The scale of the screenshake effect");
+            shakeLength = Config.Bind("Screenshake", "Screenshake Length", 3f, "The length of the screenshake effect");
+
+            Logger.LogInfo("Hiiiiiiiiiiii :3");
+            Harmony harmony = new Harmony(pluginGuid);
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerMovement), nameof(PlayerMovement.FinishClimbToSurface))]
+    public class PatchLadderFling
+    {   
+        private static void Fling(PlayerMovement instance, Vector3 flingVec) {
+            AccessTools.Field(typeof(PlayerMovement), "vMomentum").SetValue(instance, flingVec.y);
+            AccessTools.Field(typeof(PlayerMovement), "uprightMovementVector").SetValue(instance, new Vector3(flingVec.x, 0f, flingVec.y));
+        }
+
+        [HarmonyPostfix]
+        public static void Postfix(PlayerMovement __instance) {
+            int rand = Mod.rand.Next(100) + 1;
+            //Debug.Log(rand); //bug check: there was no bug im just unlucky lol
+            if (rand <= Mathf.Clamp(Mod.launchChance.Value, 0, 100)) { //launchChance% chance of getting flung
+                Fling(__instance, new Vector3(10, 0.1f, 10) * Mod.launchMultiplier.Value);
+                GameManager.instance.cameraManager.GetShakeManager().AddShake(new CameraShake(Mod.shakeFrequency.Value, Mod.shakeScale.Value, Mod.shakeLength.Value));
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# Justice For Ladder Bug mod for I Am Your Beast
+
+did you know ladders and walls fling you 27km away when you climb them sometimes (or used to depending on when youre reading this)
+this mod adds that as a feature because its (objectively) awesome
+
+You can customise:
+
+- Strength of the fling
+- Chance for the fling to happen
+- Scale, frequency and length of screenshake caused by the fling
+
+
+# Installation
+
+!!! You need [Bepinex 6](https://github.com/BepInEx/BepInEx/releases/tag/v6.0.0-pre.2) for mono !!!
+(if you have no idea what the versions mean try BepInEx-Unity.Mono-win-x64-6.0.0-pre.2 and it might work. maybe)
+
+Once bepinex is installed, put the dll into the BepInEx/plugins folder in the game's root directory.
+
+have fun :3
+
+![](https://files.catbox.moe/xiqcwl.gif)
